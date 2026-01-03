8d8ef36bed246496fb764cf148883e6119c5315d
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2026-01-03 12:18:54 -0300

    db

--- livro/models.py
+++ livro/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from datetime import date
 
 class Livros(models.Model):
     nome = models.CharField(max_length=100)
     autor = models.CharField(max_length=30)
-    co_autor = models.CharField(max_length=30)
-    data_cadastro = models.DateField()
+    co_autor = models.CharField(max_length=30, blank=True)
+    data_cadastro = models.DateField( default=date.today)
     emprestado = models.BooleanField(default=False)
-    nome_emprestimo = models.CharField(max_length=30)
-    data_emprestimo = models.DateTimeField()
-    data_devolucao = models.DateTimeField()
-    tempo_emprestimo = models.DateTimeField()
+    nome_emprestimo = models.CharField(max_length=30, blank=True)
+    data_emprestimo = models.DateTimeField(blank=True)
+    data_devolucao = models.DateTimeField(blank=True)
+    tempo_emprestimo = models.DateTimeField(blank=True)
 
     class Meta:
         verbose_name = "Livro"
